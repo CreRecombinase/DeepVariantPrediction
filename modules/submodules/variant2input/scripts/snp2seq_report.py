647e841c6375c5e31e647d1994213bd0d7d85557
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-04-24 17:26:37 -0500

    debugged

--- modules/submodules/variant2input/scripts/snp2seq_report.py
+++ modules/submodules/variant2input/scripts/snp2seq_report.py
@@ -5,16 +5,18 @@ dtf = pd.read_csv(snakemake.input.a, sep = '\t')
 match_allele1 = (dtf.Allele1 == dtf.Ref).sum()
 match_allele2 = (dtf.Allele2 == dtf.Ref).sum()
 total = dtf.shape[0]
+notmatch = total - match_allele1 - match_allele2
 
 report("""
 Variant Formatting Report
 =========================
 
-The allele1, allele2, reference allele are summarized here table_
+The unmatched allele1, allele2, reference allele are summarized here table_
 The formatting step is done by `{snakemake.params.formatting}`
 
 * total variants = {total}
 * variants match allele1 = {match_allele1}
 * variants match allele2 = {match_allele2}
+* number of unmatched variants = {notmatch}
 
-""", snakemake.output.o, table=snakemake.input.a)
+""", snakemake.output.o, table=snakemake.input.a2)
