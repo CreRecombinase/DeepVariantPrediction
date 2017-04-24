3eb4cec5996388983fa14206950cd09397001163
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-04-24 16:48:09 -0500

    added log to extract fasta

--- modules/submodules/variant2input/scripts/snp2seq_extract.py
+++
@@ -1,8 +0,0 @@
-import sys
-if 'scripts' not in sys.path:
-    sys.path.insert(0, 'scripts')
-import my_python
-# extract sequence from genome
-cmd = '''bedtools getfasta -fi {genome} -bed {input} -fo {out} \
--name -tab'''.format(genome=snakemake.params.assembly, input=snakemake.input.a1, out=snakemake.output.out)
-my_python.myOsSystem(cmd, False)
