ba6006bc75ae808149d4c12d28312016fd800d0c
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-04-25 16:10:47 -0500

    refine

--- scripts/score2feather.py
+++ scripts/score2feather.py
@@ -16,6 +16,7 @@ parser.add_argument('--label_num', nargs='+', type=int, help='''
     The column number of each label (1-based)
     ''')
 parser.add_argument('--id')
+parser.add_argument('--rsid')
 parser.add_argument('--out')
 args = parser.parse_args()
 
@@ -28,6 +29,7 @@ if 'scripts' not in sys.path:
 import my_python
 import feather
 ids = np.loadtxt(args.id)
+rsid = np.loadtxt(args.rsid)
 a1 = my_python.getData(args.allele1, 'y_pred')
 a2 = my_python.getData(args.allele2, 'y_pred')
 total_table = pd.DataFrame()
@@ -36,6 +38,7 @@ for i in range(len(args.label_num)):
     value2 = a2[:,args.label_num[i] - 1]
     sub_table = pd.DataFrame({
 				'Varient.ID' : ids.astype(int),
+				'rsID' : rsid.astype(str)
 				'Annotation' : args.label_name[i],
 				'Allele1' : value1,
                 'Allele2' : value2,
